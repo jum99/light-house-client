47f7a80aa02889a081c52b7e8af899f2f5e00027
jum99 <ejumibegum@gmail.com>
2021-11-13 23:44:54 +0600

    last-commit-done

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,7 @@ import '../style/App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
@@ -20,22 +19,24 @@ import AboutUs from './sections/AboutUs';
 import NotFound from './sections/NotFound';
 
 
+
 const App = () => {
   return (
     <AuthProvider>
       <Router>
         <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route path="/home"><Home /></Route>
           <PrivateRoute path="/dashboard"><RouteDiv /></PrivateRoute>
           <Route path="/login"><Login /></Route>
           <Route path="/signUp"><SignUp /></Route>
           <PrivateRoute path="/explore"><Explore /></PrivateRoute>
           <PrivateRoute path="/purchase/:Id"><SingleProduct /></PrivateRoute>
-          <Route path="/home"><Home /></Route>
+
           <Route path="/about"><AboutUs /></Route>
           <Route path="*">
             <NotFound />
           </Route>
-          <Route exact path="/"><Home /></Route>
         </Switch>
       </Router>
     </AuthProvider>
